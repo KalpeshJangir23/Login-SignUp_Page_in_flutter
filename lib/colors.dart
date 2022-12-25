7913a12e18fd8b96270945a99732aeee8fc47da2
LUNAGOK <21LUNAGOK@gmail.com>
2022-12-25 21:36:28 +0530

    01

---
+++ lib/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
+const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
+const secondColor = Color.fromRGBO(0, 149, 246, 1);
+const mainBackground = Color.fromRGBO(34, 35, 41, 1);
+const secondaryColor = Color.fromRGBO(177, 228, 159, 1);
+const textColor = Colors.white;
+Text textt = Text(
+  'Enter your Email',
+  style: GoogleFonts.inconsolata(
+    fontSize: 12,
+    color: secondColor,
+  ),
+);
